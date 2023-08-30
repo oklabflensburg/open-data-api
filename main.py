7bdb7a16d440775c3f9295cdfba19dd4e21d2a46
Aurelius Wendelken <webtobesocial@gmail.com>
2023-08-30 15:58:09 +0200

    add project and version prefix to route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
@@ -11,6 +11,7 @@ import service
 
 app = FastAPI()
 Base = declarative_base()
+router = APIRouter(prefix='/sozialatlas/v1')
 
 
 @app.on_event('startup')
@@ -27,7 +28,7 @@ async def get_session() -> AsyncSession:
 
 
 
-@app.get('/districts', response_model=list[schemas.District])
+@router.get('/districts', response_model=list[schemas.District])
 async def get_all_districts(session: AsyncSession = Depends(get_session)):
     districts = await service.get_districts(session)
 
@@ -35,28 +36,30 @@ async def get_all_districts(session: AsyncSession = Depends(get_session)):
 
 
 '''
-@app.get('/household/types', response_model=list[schemas.])
-@app.get('/residents/agegroups', response_model=list[schemas.])
-@app.get('/residents/nongermans', response_model=list[schemas.])
-@app.get('/residents/debtcounseling', response_model=list[schemas.])
-@app.get('/residents/education/support', response_model=list[schemas.])
-@app.get('/district/residents', response_model=list[schemas.])
-@app.get('/district/residents/births', response_model=list[schemas.])
-@app.get('/district/residents/employed', response_model=list[schemas.])
-@app.get('/district/residents/ageratio', response_model=list[schemas.])
-@app.get('/district/residents/basicbenefits', response_model=list[schemas.])
-@app.get('/district/residents/age18tounder65', response_model=list[schemas.])
-@app.get('/district/residents/ageunder18', response_model=list[schemas.])
-@app.get('/district/residents/age65andabove', response_model=list[schemas.])
-@app.get('/district/residents/agegroups', response_model=list[schemas.])
-@app.get('/district/residents/unemployed', response_model=list[schemas.])
-@app.get('/district/residents/unemployed/categorized', response_model=list[schemas.])
-@app.get('/district/residents/beneficiaries', response_model=list[schemas.])
-@app.get('/district/residents/beneficiaries/inactive', response_model=list[schemas.])
-@app.get('/district/residents/beneficiaries/characteristics', response_model=list[schemas.])
-@app.get('/district/residents/beneficiaries/age15tounder65', response_model=list[schemas.])
-@app.get('/district/residents/migration/background', response_model=list[schemas.])
-@app.get('/district/residents/housing/assistance', response_model=list[schemas.])
-@app.get('/district/residents/housing/benefit', response_model=list[schemas.])
-@app.get('/district/residents/risk/homelessness', response_model=list[schemas.])
+@router.get('/household/types', response_model=list[schemas.])
+@router.get('/residents/agegroups', response_model=list[schemas.])
+@router.get('/residents/nongermans', response_model=list[schemas.])
+@router.get('/residents/debtcounseling', response_model=list[schemas.])
+@router.get('/residents/education/support', response_model=list[schemas.])
+@router.get('/district/residents', response_model=list[schemas.])
+@router.get('/district/residents/births', response_model=list[schemas.])
+@router.get('/district/residents/employed', response_model=list[schemas.])
+@router.get('/district/residents/ageratio', response_model=list[schemas.])
+@router.get('/district/residents/basicbenefits', response_model=list[schemas.])
+@router.get('/district/residents/age18tounder65', response_model=list[schemas.])
+@router.get('/district/residents/ageunder18', response_model=list[schemas.])
+@router.get('/district/residents/age65andabove', response_model=list[schemas.])
+@router.get('/district/residents/agegroups', response_model=list[schemas.])
+@router.get('/district/residents/unemployed', response_model=list[schemas.])
+@router.get('/district/residents/unemployed/categorized', response_model=list[schemas.])
+@router.get('/district/residents/beneficiaries', response_model=list[schemas.])
+@router.get('/district/residents/beneficiaries/inactive', response_model=list[schemas.])
+@router.get('/district/residents/beneficiaries/characteristics', response_model=list[schemas.])
+@router.get('/district/residents/beneficiaries/age15tounder65', response_model=list[schemas.])
+@router.get('/district/residents/migration/background', response_model=list[schemas.])
+@router.get('/district/residents/housing/assistance', response_model=list[schemas.])
+@router.get('/district/residents/housing/benefit', response_model=list[schemas.])
+@router.get('/district/residents/risk/homelessness', response_model=list[schemas.])
 '''
+
+app.include_router(router)
