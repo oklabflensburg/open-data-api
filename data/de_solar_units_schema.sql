a8d9866b6fc92afb9137993fb93e1b36f5c8842e
Aurelius Wendelken <webtobesocial@gmail.com>
2024-11-30 12:22:48 +0100

    added fields to endpoint solar and correct types

--- data/de_solar_units_schema.sql
+++ data/de_solar_units_schema.sql
@@ -3,40 +3,204 @@ DROP TABLE IF EXISTS de_solar_units CASCADE;
 
 CREATE TABLE IF NOT EXISTS de_solar_units (
     id SERIAL PRIMARY KEY,
+
+    -- MaStR-Nummer der Einheit
     unit_registration_number VARCHAR,
-    last_update TIMESTAMP WITH TIME ZONE,
+
+    -- Datum der letzten Aktualisierung an diesem Objekt
+    last_update TIMESTAMP,
+
+    -- MaStR-Nummer der Lokation
     location_registration_number VARCHAR,
+
+    -- Der Status der letzten Netzbetreiberprüfung, insofern eine durchgeführt wurde
     network_operator_audit_id INT,
+
+    -- MaStR-Nummer des Betreibers der Einheit
     operator_registration_number VARCHAR,
+
+    -- Standort der Einheit: Land
     country_id INT,
+
+    -- Standort der Einheit: Bundesland
     state_id INT,
+
+    -- Standort der Einheit: Landkreis
     district VARCHAR,
+
+    -- Standort der Einheit: Gemeinde
     municipality_name VARCHAR,
+
+    -- Standort der Einheit: Gemeindeschlüssel
     municipality_key VARCHAR,
+
+    -- Standort der Einheit: Postleitzahl
     postcode VARCHAR,
+
+    -- Standort der Einheit: Ort
     city VARCHAR,
+
+    -- Bürgerenergieeigenschaft der Einheit
+    citizen_energy BOOLEAN,
+
+    -- Datum der letzten Netzbetreiberprüfung, insofern eine durchgeführt wurde
+    network_operator_inspection_date TIMESTAMP,
+
+    -- Datum der endgültigen Stilllegung der Einheit
+    final_decommissioning_date TIMESTAMP,
+
+    -- Beginn der vorläufigen Stilllegung der Einheit
+    temporary_decommissioning_date TIMESTAMP,
+
+    -- Datum der Wiederaufnahme des Betriebs
+    operation_resumption_date TIMESTAMP,
+
+    -- Geplantes Inbetriebnahmedatum der Stromerzeugungsseinheit
+    planned_commissioning_date TIMESTAMP,
+
+    -- Angaben über optional vorhandene MaStR-Nummer aus der Bestandsanlagenverwaltung
+    legacy_system_registration_number VARCHAR,
+
+    -- MaStR-Nummer des ehemaligen Betreibers der Einheit
+    former_operator_registration_number VARCHAR,
+
+    -- Datum des realen Betreiberwechsels
+    actual_operator_change_date TIMESTAMP,
+
+    -- Datum der Registrierung des Betreiberwechsels
+    operator_change_registration_date TIMESTAMP,
+
+    -- W-Code der Stromerzeugungseinheit
+    w_code VARCHAR,
+
+    -- Displayname des W-EIC
+    w_code_display_name VARCHAR,
+
+    -- Bundesnetzagentur-Kraftwerksnummer
+    power_plant_number VARCHAR,
+
+    -- Die Stromerzeugungseinheit ist an ein Höchst- oder Hochspannungsnetz angeschlossen
+    high_voltage_connection VARCHAR,
+
+    -- Fernsteuerbarkeit der Einheit durch einen Direktvermarkter
+    remote_control_direct_marketer VARCHAR,
+
+    -- Standort der Einheit: Gemarkung
+    cadastral_district VARCHAR,
+
+    -- Standort der Einheit: Flur und/oder Flurstücke
+    plots_or_parcel_numbers VARCHAR,
+
+    -- Standort der Einheit: Straße
+    street VARCHAR,
+
+    -- Angabe, dass die angegebene Strasse nicht aus dem BKG-Adressdatenbestand stammt
+    street_not_found VARCHAR,
+
+    -- Standort der Einheit: Hausnummer
+    housenumber VARCHAR,
+
+    -- Angabe, dass die angegebene Hausnummer nicht aus dem BKG-Adressdatenbestand stammt
+    housenumber_not_found VARCHAR,
+
+    -- Standort der Einheit: Adresszusatz
+    address_addition VARCHAR,
+
+    -- Koordinaten der Einheit: Längengrad
+    longitude NUMERIC,
+
+    -- Koordinaten der Einheit: Breitengrad
+    latitude NUMERIC,
+
+    -- Koordinaten der Einheit: UTM-Zonenwert
+    utm_zone INT,
+
+    -- Koordinaten der Einheit: UTM-Ost
+    utm_east NUMERIC,
+
+    -- Koordinaten der Einheit: UTM-Nord
+    utm_north NUMERIC,
+
+    -- Koordinaten der Einheit: Gauß-Krüger-Hochwert
+    gauss_kruger_north NUMERIC,
+
+    -- Koordinaten der Einheit: Gauß-Krüger-Rechtswert
+    gauss_kruger_east NUMERIC,
+
+    -- Schwarzstartfähigkeit der Einheit
+    black_start_capability BOOLEAN,
+
+    -- Inselbetriebsfähigkeit der Einheit
+    island_operation_capability BOOLEAN,
+
+    -- Marktpartner-ID des Einsatzverantwortlichen
+    responsible_partner_number VARCHAR,
+
+    -- Registrierungsdatum der Einheit
     registration_date TIMESTAMP,
+
+    -- Datum der Inbetriebnahme
     commissioning_date TIMESTAMP,
+
+    -- Systemstatus der Einheit
     unit_system_status_id INT,
+
+    -- Betriebsstatus der Einheit
     unit_operational_status_id INT,
-    not_present_migrated_units VARCHAR,
+
+    -- Angabe über das Nichtvorhandensein in den migrierten Einheiten
+    not_present_migrated_units BOOLEAN,
+
+    -- Vom Betreiber frei wählbare Bezeichnung der Stromerzeugungseinheit
     unit_name VARCHAR,
-    weic_not_available VARCHAR,
-    power_plant_number_not_available VARCHAR,
+
+    weic_not_available BOOLEAN,
+
+    power_plant_number_not_available BOOLEAN,
+
+    -- Energieträger der Einheit
     energy_source_id INT,
+
+    -- Bruttoleistung in kW
     gross_capacity NUMERIC,
+
+    -- Nettonennleistung in kW
     net_nominal_capacity NUMERIC,
-    remote_controllability NUMERIC,
+
+    -- Fernsteuerbarkeit der Einheit durch einen Netzbetreiber
+    remote_controllability BOOLEAN,
+
+    -- Volleinspeisung oder TeileinspeisungEigenverbrauch
     supply_type_id INT,
+
+    -- Wechselrichterleistung der Stromerzeugungseinheit
     assigned_active_power_inverter NUMERIC,
+
+    -- Anzahl der Module dieser Stromerzeugungseinheit
     amount_modules INT,
+
+    -- Errichtungsort der Stromerzeugungseinheit
     location_id INT,
+
+    -- Die Leistung der Stromerzeugungseinheit ist auf einen bestimmten prozentualen Leistungsanteil begrenzt
     power_limitation_id INT,
-    uniform_orientation_tilt_angle_id INT,
+
+    -- Angabe, ob einheitliche Ausrichtung und Neigungswinkel bestehen
+    uniform_orientation_tilt_angle BOOLEAN,
+
+    -- Die Ausrichtung bezeichnet die Himmelsrichtung
     main_orientation_id INT,
+
+    -- Der Neigungswinkel bezeichnet den Winkel gegenüber der Horizontalen
     main_orientation_tilt_angle_id INT,
+
+    -- Vorrangige Nutzung des in Anspruch genommenen Gebäudes
     usage_area_id INT,
-    eeg_registration_number VARCHAR
+
+    -- MaStR-Nummer der EEG-Anlage
+    eeg_registration_number VARCHAR,
+
+    wkb_geometry GEOMETRY(POINT, 4326)
 );
 
 
@@ -45,3 +209,6 @@ CREATE INDEX IF NOT EXISTS idx_unit_solar_municipality_key ON de_solar_units (mu
 
 -- UNIQUE INDEX
 CREATE UNIQUE INDEX IF NOT EXISTS idx_unq_solar_unit_reg_num ON de_solar_units (unit_registration_number);
+
+-- GEOMETRY INDEX
+CREATE INDEX IF NOT EXISTS idx_solar_unit_geometry ON de_solar_units USING GIST (wkb_geometry);
--- tools/insert_solar_units.py
+++ tools/insert_solar_units.py
@@ -4,8 +4,10 @@ import click
 import traceback
 import logging as log
 import psycopg2
+import psycopg2.extras
 
 from datetime import datetime
+from shapely.geometry import Point
 from dotenv import load_dotenv
 from pathlib import Path
 from lxml import etree
@@ -59,6 +61,10 @@ def parse_date(s):
     return datetime.strptime(s, '%Y-%m-%d')
 
 
+def parse_bool(v):
+    return v.lower() in ('yes', 'true', 't', '1')
+
+
 def parse_value(elem, tag_name, conversion_func=None):
     tag = elem.find(tag_name)
 
@@ -74,14 +80,27 @@ def parse_value(elem, tag_name, conversion_func=None):
 def insert_row(cur, row):
     columns = [
         'unit_registration_number', 'last_update', 'location_registration_number',
-        'network_operator_audit_id', 'operator_registration_number', 'country_id', 'state_id',
-        'district', 'municipality_name', 'municipality_key', 'postcode', 'city',
-        'registration_date', 'commissioning_date', 'unit_system_status_id', 'unit_operational_status_id',
+        'network_operator_audit_id', 'operator_registration_number', 'country_id',
+        'state_id', 'district', 'municipality_name', 'municipality_key',
+        'postcode', 'city', 'citizen_energy', 'network_operator_inspection_date',
+        'final_decommissioning_date', 'temporary_decommissioning_date',
+        'operation_resumption_date', 'planned_commissioning_date',
+        'legacy_system_registration_number', 'former_operator_registration_number',
+        'actual_operator_change_date', 'operator_change_registration_date',
+        'w_code', 'w_code_display_name', 'power_plant_number', 'high_voltage_connection',
+        'remote_control_direct_marketer', 'cadastral_district', 'plots_or_parcel_numbers',
+        'street', 'street_not_found', 'housenumber', 'housenumber_not_found',
+        'address_addition', 'longitude', 'latitude', 'utm_zone', 'utm_east', 'utm_north',
+        'gauss_kruger_north', 'gauss_kruger_east', 'black_start_capability',
+        'island_operation_capability', 'responsible_partner_number', 'registration_date',
+        'commissioning_date', 'unit_system_status_id', 'unit_operational_status_id',
         'not_present_migrated_units', 'unit_name', 'weic_not_available',
-        'power_plant_number_not_available', 'energy_source_id', 'gross_capacity', 'net_nominal_capacity',
-        'remote_controllability', 'supply_type_id', 'assigned_active_power_inverter',
-        'amount_modules', 'location_id', 'power_limitation_id', 'uniform_orientation_tilt_angle_id',
-        'main_orientation_id', 'main_orientation_tilt_angle_id', 'usage_area_id', 'eeg_registration_number'
+        'power_plant_number_not_available', 'energy_source_id', 'gross_capacity',
+        'net_nominal_capacity', 'remote_controllability', 'supply_type_id',
+        'assigned_active_power_inverter', 'amount_modules', 'location_id',
+        'power_limitation_id', 'uniform_orientation_tilt_angle', 'main_orientation_id',
+        'main_orientation_tilt_angle_id', 'usage_area_id', 'eeg_registration_number',
+        'wkb_geometry'
     ]
 
     sql = f'''
@@ -106,41 +125,208 @@ def read_solar_units(conn, src):
 
     for event, elem in context:
         unit = {
+            # MaStR-Nummer der Einheit
             'unit_registration_number': parse_value(elem, 'EinheitMastrNummer'),
+
+            # Datum der letzten Aktualisierung an diesem Objekt
             'last_update': parse_value(elem, 'DatumLetzteAktualisierung', parse_datetime),
+
+            # MaStR-Nummer der Lokation
             'location_registration_number': parse_value(elem, 'LokationMaStRNummer'),
+
+            # Der Status der letzten Netzbetreiberprüfung, insofern eine durchgeführt wurde
             'network_operator_audit_id': parse_value(elem, 'NetzbetreiberpruefungStatus'),
+
+            # MaStR-Nummer des Betreibers der Einheit
             'operator_registration_number': parse_value(elem, 'AnlagenbetreiberMastrNummer'),
+
+            # Standort der Einheit: Land
             'country_id': parse_value(elem, 'Land', int),
+
+            # Standort der Einheit: Bundesland
             'state_id': parse_value(elem, 'Bundesland', int),
+
+            # Standort der Einheit: Landkreis
             'district': parse_value(elem, 'Landkreis'),
+
+            # Standort der Einheit: Gemeinde
             'municipality_name': parse_value(elem, 'Gemeinde'),
+
+            # Standort der Einheit: Gemeindeschlüssel
             'municipality_key': parse_value(elem, 'Gemeindeschluessel'),
+
+            # Standort der Einheit: Postleitzahl
             'postcode': parse_value(elem, 'Postleitzahl'),
+
+            # Standort der Einheit: Ort
             'city': parse_value(elem, 'Ort'),
+
+            # Bürgerenergieeigenschaft der Einheit
+            'citizen_energy': parse_value(elem, 'Buergerenergie'),
+
+            # Datum der letzten Netzbetreiberprüfung, insofern eine durchgeführt wurde
+            'network_operator_inspection_date': parse_value(elem, 'NetzbetreiberpruefungDatum', parse_date),
+
+            # Datum der endgültigen Stilllegung der Einheit
+            'final_decommissioning_date': parse_value(elem, 'DatumEndgueltigeStilllegung', parse_date),
+
+            # Beginn der vorläufigen Stilllegung der Einheit
+            'temporary_decommissioning_date': parse_value(elem, 'DatumBeginnVoruebergehendeStilllegung', parse_date),
+
+            # Datum der Wiederaufnahme des Betriebs
+            'operation_resumption_date': parse_value(elem, 'DatumWiederaufnahmeBetrieb', parse_date),
+
+            # Geplantes Inbetriebnahmedatum der Stromerzeugungsseinheit
+            'planned_commissioning_date': parse_value(elem, 'GeplantesInbetriebnahmedatum', parse_date),
+
+            # Angaben über optional vorhandene MaStR-Nummer aus der Bestandsanlagenverwaltung
+            'legacy_system_registration_number': parse_value(elem, 'BestandsanlageMastrNummer'),
+
+            # MaStR-Nummer des ehemaligen Betreibers der Einheit
+            'former_operator_registration_number': parse_value(elem, 'AltAnlagenbetreiberMastrNummer'),
+
+            # Datum des realen Betreiberwechsels
+            'actual_operator_change_date': parse_value(elem, 'DatumDesBetreiberwechsels', parse_date),
+
+            # Datum der Registrierung des Betreiberwechsels
+            'operator_change_registration_date': parse_value(elem, 'DatumRegistrierungDesBetreiberwechsels', parse_date),
+
+            # W-Code der Stromerzeugungseinheit
+            'w_code': parse_value(elem, 'Weic'),
+
+            # Displayname des W-EIC
+            'w_code_display_name': parse_value(elem, 'WeicDisplayName'),
+
+            # Bundesnetzagentur-Kraftwerksnummer
+            'power_plant_number': parse_value(elem, 'Kraftwerksnummer'),
+
+            # Die Stromerzeugungseinheit ist an ein Höchst- oder Hochspannungsnetz angeschlossen
+            'high_voltage_connection': parse_value(elem, 'AnschlussAnHoechstOderHochSpannung'),
+
+            # Fernsteuerbarkeit der Einheit durch einen Direktvermarkter
+            'remote_control_direct_marketer': parse_value(elem, 'FernsteuerbarkeitDv'),
+
+            # Standort der Einheit: Gemarkung
+            'cadastral_district': parse_value(elem, 'Gemarkung'),
+
+            # Standort der Einheit: Flur und/oder Flurstücke
+            'plots_or_parcel_numbers': parse_value(elem, 'FlurFlurstuecknummern'),
+
+            # Standort der Einheit: Straße
+            'street': parse_value(elem, 'Straße'),
+
+            # Angabe, dass die angegebene Strasse nicht aus dem BKG-Adressdatenbestand stammt
+            'street_not_found': parse_value(elem, 'StrasseNichtGefunden'),
+
+            # Standort der Einheit: Hausnummer
+            'housenumber': parse_value(elem, 'Hausnummer'),
+
+            # Angabe, dass die angegebene Hausnummer nicht aus dem BKG-Adressdatenbestand stammt
+            'housenumber_not_found': parse_value(elem, 'HausnummerNichtGefunden'),
+
+            # Standort der Einheit: Adresszusatz
+            'address_addition': parse_value(elem, 'Adresszusatz'),
+
+            # Koordinaten der Einheit: Längengrad
+            'longitude': parse_value(elem, 'Laengengrad', float),
+
+            # Koordinaten der Einheit: Breitengrad
+            'latitude': parse_value(elem, 'Breitengrad', float),
+
+            # Koordinaten der Einheit: UTM-Zonenwert
+            'utm_zone': parse_value(elem, 'UtmZonenwert', int),
+
+            # Koordinaten der Einheit: UTM-Ost
+            'utm_east': parse_value(elem, 'UtmEast', float),
+
+            # Koordinaten der Einheit: UTM-Nord
+            'utm_north': parse_value(elem, 'UtmNorth', float),
+
+            # Koordinaten der Einheit: Gauß-Krüger-Hochwert
+            'gauss_kruger_north': parse_value(elem, 'GaussKruegerHoch', float),
+
+            # Koordinaten der Einheit: Gauß-Krüger-Rechtswert
+            'gauss_kruger_east': parse_value(elem, 'GaussKruegerRechts', float),
+
+            # Schwarzstartfähigkeit der Einheit
+            'black_start_capability': parse_value(elem, 'Schwarzstartfaehigkeit', parse_bool),
+
+            # Inselbetriebsfähigkeit der Einheit
+            'island_operation_capability': parse_value(elem, 'Inselbetriebsfaehigkeit', parse_bool),
+
+            # Marktpartner-ID des Einsatzverantwortlichen
+            'responsible_partner_number': parse_value(elem, 'Einsatzverantwortlicher'),
+
+            # Registrierungsdatum der Einheit
             'registration_date': parse_value(elem, 'Registrierungsdatum', parse_date),
+
+            # Datum der Inbetriebnahme
             'commissioning_date': parse_value(elem, 'Inbetriebnahmedatum', parse_date),
+
+            # Systemstatus der Einheit
             'unit_system_status_id': parse_value(elem, 'EinheitSystemstatus', int),
+
+            # Betriebsstatus der Einheit
             'unit_operational_status_id': parse_value(elem, 'EinheitBetriebsstatus', int),
-            'not_present_migrated_units': parse_value(elem, 'NichtVorhandenInMigriertenEinheiten'),
+
+            # Angabe über das Nichtvorhandensein in den migrierten Einheiten
+            'not_present_migrated_units': parse_value(elem, 'NichtVorhandenInMigriertenEinheiten', parse_bool),
+
+            # Vom Betreiber frei wählbare Bezeichnung der Stromerzeugungseinheit
             'unit_name': parse_value(elem, 'NameStromerzeugungseinheit'),
-            'weic_not_available': parse_value(elem, 'Weic_nv'),
-            'power_plant_number_not_available': parse_value(elem, 'Kraftwerksnummer_nv'),
+
+            'weic_not_available': parse_value(elem, 'Weic_nv', parse_bool),
+
+            'power_plant_number_not_available': parse_value(elem, 'Kraftwerksnummer_nv', parse_bool),
+
+            # Energieträger der Einheit
             'energy_source_id': parse_value(elem, 'Energietraeger', int),
+
+            # Bruttoleistung in kW
             'gross_capacity': parse_value(elem, 'Bruttoleistung', float),
-            'net_nominal_capacity': parse_value(elem, 'Nettonennleistung'),
-            'remote_controllability': parse_value(elem, 'FernsteuerbarkeitNb'),
+
+            # Nettonennleistung in kW
+            'net_nominal_capacity': parse_value(elem, 'Nettonennleistung', float),
+
+            # Fernsteuerbarkeit der Einheit durch einen Netzbetreiber
+            'remote_controllability': parse_value(elem, 'FernsteuerbarkeitNb', parse_bool),
+
+            # Volleinspeisung oder TeileinspeisungEigenverbrauch
             'supply_type_id': parse_value(elem, 'Einspeisungsart', int),
-            'assigned_active_power_inverter': parse_value(elem, 'ZugeordneteWirkleistungWechselrichter'),
-            'amount_modules': parse_value(elem, 'AnzahlModule'),
+
+            # Wechselrichterleistung der Stromerzeugungseinheit
+            'assigned_active_power_inverter': parse_value(elem, 'ZugeordneteWirkleistungWechselrichter', float),
+
+            # Anzahl der Module dieser Stromerzeugungseinheit
+            'amount_modules': parse_value(elem, 'AnzahlModule', int),
+
+            # Errichtungsort der Stromerzeugungseinheit
             'location_id': parse_value(elem, 'Lage', int),
+
+            # Die Leistung der Stromerzeugungseinheit ist auf einen bestimmten prozentualen Leistungsanteil begrenzt
             'power_limitation_id': parse_value(elem, 'Leistungsbegrenzung', int),
-            'uniform_orientation_tilt_angle_id': parse_value(elem, 'EinheitlicheAusrichtungUndNeigungswinkel'),
+
+            # Angabe, ob einheitliche Ausrichtung und Neigungswinkel bestehen
+            'uniform_orientation_tilt_angle': parse_value(elem, 'EinheitlicheAusrichtungUndNeigungswinkel', parse_bool),
+
+            # Die Ausrichtung bezeichnet die Himmelsrichtung
             'main_orientation_id': parse_value(elem, 'Hauptausrichtung', int),
+
+            # Der Neigungswinkel bezeichnet den Winkel gegenüber der Horizontalen
             'main_orientation_tilt_angle_id': parse_value(elem, 'HauptausrichtungNeigungswinkel', int),
+
+            # Vorrangige Nutzung des in Anspruch genommenen Gebäudes
             'usage_area_id': parse_value(elem, 'Nutzungsbereich', int),
-            'eeg_registration_number': parse_value(elem, 'EegMaStRNummer')
+
+            # MaStR-Nummer der EEG-Anlage
+            'eeg_registration_number': parse_value(elem, 'EegMaStRNummer'),
+
+            'wkb_geometry': None
         }
+
+        if unit['longitude'] and unit['latitude']:
+            point = Point(unit['longitude'], unit['latitude'])
+            unit['wkb_geometry'] = point.wkb
 
         insert_row(cur, unit)
         elem.clear()
