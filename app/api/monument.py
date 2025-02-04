d83fe62511448815b64dad1a88279020f9f51dde
Aurelius Wendelken <webtobesocial@gmail.com>
2025-02-04 11:59:57 +0100

    add endpoint to fetch monument detail by slug

--- app/api/monument.py
+++ app/api/monument.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
 from ..dependencies import get_session
-from ..services.monument import get_monument_by_id, get_monument_geometries_by_bbox
+from ..services.monument import get_monument_by_id, get_monument_by_slug, get_monument_geometries_by_bbox
 
 
 route_monument = APIRouter(prefix='/monument/v1')
@@ -31,12 +31,29 @@ async def fetch_monument_by_monument_id(monument_id: int, session: AsyncSession 
 
 
 @route_monument.get(
+    '/detail',
+    response_model=List,
+    tags=['Denkmalliste']
+)
+async def fetch_monument_by_slug(slug: str, session: AsyncSession = Depends(get_session)):
+    rows = await get_monument_by_slug(session, slug)
+    response = jsonable_encoder(rows)
+
+    if len(response) == 0:
+        raise HTTPException(status_code=404, detail=f'No matches found for monument slug {slug}')
+
+    return JSONResponse(content=response)
+
+
+
+@route_monument.get(
     '/geometries',
     response_model=dict,
     tags=['Denkmalliste']
 )
 async def fetch_monument_geometries_by_bbox(xmin: float, ymin: float, xmax: float, ymax: float, session: AsyncSession = Depends(get_session)):
     rows = await get_monument_geometries_by_bbox(session, xmin, ymin, xmax, ymax)
+
     features = [
         Feature(
             id=row['id'],
--- app/services/monument.py
+++ app/services/monument.py
@@ -6,6 +6,70 @@ from ..utils.validators import validate_positive_int32
 
 
 
+async def get_monument_by_slug(session: AsyncSession, slug: str):
+    stmt = text('''
+    WITH monument_reasons AS (
+        SELECT
+            mxr.monument_id,
+            string_agg(mr.label, ', ') AS reason_labels
+        FROM
+            sh_monument_x_reason AS mxr
+        JOIN
+            sh_monument_reason AS mr
+        ON
+            mxr.reason_id = mr.id
+        GROUP BY
+            mxr.monument_id
+    ),
+    monument_scopes AS (
+        SELECT
+            mxs.monument_id,
+            string_agg(ms.label, ', ') AS scope_labels
+        FROM
+            sh_monument_x_scope AS mxs
+        JOIN
+            sh_monument_scope AS ms
+        ON
+            mxs.scope_id = ms.id
+        GROUP BY
+            mxs.monument_id
+    )
+    SELECT
+        ST_AsGeoJSON(m.wkb_geometry, 15)::jsonb AS geojson,
+        m.object_id,
+        m.slug,
+        m.street,
+        m.housenumber,
+        m.postcode,
+        m.city,
+        m.image_url,
+        m.designation,
+        m.description,
+        m.monument_type,
+        r.reason_labels AS monument_reason,
+        s.scope_labels AS monument_scope
+    FROM
+        sh_monument AS m
+    LEFT JOIN
+        monument_reasons AS r
+    ON
+        m.id = r.monument_id
+    LEFT JOIN
+        monument_scopes AS s
+    ON
+        m.id = s.monument_id
+    WHERE
+        m.slug = :slug
+    ''')
+
+    sql = stmt.bindparams(slug=slug)
+    result = await session.execute(sql)
+    rows = result.mappings().all()
+
+    return [dict(row) for row in rows]
+
+
+
 async def get_monument_geometries_by_bbox(session: AsyncSession, xmin: float, ymin: float, xmax: float, ymax: float):
     stmt = text('''
     SELECT id, street, housenumber, ST_AsGeoJSON(wkb_geometry) AS geom
