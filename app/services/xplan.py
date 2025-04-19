875b4734e64f125d0a5267a098fbf0a2d77eb1ba
Aurelius Wendelken <webtobesocial@gmail.com>
2025-04-19 17:41:08 +0200

    add additional output to api endpoint

--- app/services/xplan.py
+++ app/services/xplan.py
@@ -9,6 +9,14 @@ async def get_plan_by_lat_lng(
 ):
     stmt = text('''
     SELECT
+        b.name AS plan_name,
+        b.nummer AS plan_number,
+        b.gemeinde AS municipality,
+        CASE
+            WHEN ST_Area(ST_Transform(b.geometry, 3587)) < 10000 THEN
+                ST_Area(ST_Transform(b.geometry, 3587)) || ' m²'
+            ELSE ST_Area(ST_Transform(b.geometry, 3587)) / 10000 || ' ha'
+        END AS shape_area,
         ST_AsGeoJSON(b.geometry, 15)::jsonb AS geojson
     FROM
         xplan.bp_plan AS b
